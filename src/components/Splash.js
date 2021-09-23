207f07098f30b3d97adc7f61498251699dfa3cee
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-23 11:14:44 +0330

    modify files

---
+++ src/components/Splash.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+
+const Splash = (props) => {
+    return (
+        <View style={{flex: 1, backgroundColor: '#f39c12', justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Splash Page</Text>
+        </View>
+    );
+}
+
+export default Splash;

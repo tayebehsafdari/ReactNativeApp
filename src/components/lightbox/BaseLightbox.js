e006f7add6f140f3246eb2fa5d030a8cf72b9f72
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-24 16:16:50 +0330

    modify files

---
+++ src/components/lightbox/BaseLightbox.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import {View, Text, Animated} from 'react-native';
+// import {Container, Text, Spinner} from 'native-base';
+import styles from "../assets/css";
+
+const BaseLightbox = (props) => {
+    const [opacity, setOpacity] = useState(new Animated.Value(0));
+
+    useEffect(() => {
+        Animated.timing(opacity, {
+            toValue: 1,
+            duration: 100
+        }).start();
+    }, [opacity]);
+
+    return (
+        <Animated.View style={{}}>
+        </Animated.View>
+    );
+}
+
+export default BaseLightbox;

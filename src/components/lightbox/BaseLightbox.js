b53801f86be4a4d43f7be5f5d8e6f839dd1e4aa0
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-09-24 19:04:29 +0330

    modify files

--- src/components/lightbox/BaseLightbox.js
+++ src/components/lightbox/BaseLightbox.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {Animated, Dimensions} from 'react-native';
+import {Animated, Dimensions, StyleSheet} from 'react-native';
 import {View, Button, Icon} from 'native-base';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import {Actions} from 'react-native-router-flux';
 
 const {width: deviceWidth, height: deviceHeight} = Dimensions.get('window');
 
@@ -11,10 +12,15 @@ const BaseLightbox = (props) => {
     useEffect(() => {
         Animated.timing(opacity, {
             toValue: 1,
-            duration: 100
+            duration: 200
         }).start();
     }, [opacity]);
-
+    const close = () => {
+        Animated.timing(opacity, {
+            toValue: 0,
+            duration: 200
+        }).start(Actions.pop);
+    };
     const _renderLightbox = () => {
         const {children, verticalPercent = 1, horizontalPercent = 1} = props;
         const width = verticalPercent ? deviceWidth * verticalPercent : deviceWidth;
@@ -29,12 +35,12 @@ const BaseLightbox = (props) => {
                 borderRadius: 4
             }}>
                 {children}
-                <Button transparent style={{position: 'absolute', top: 0, left: 0}}>
+                <Button transparent style={{position: 'absolute', top: 0, left: 0}} onPress={() => close()}>
                     <Icon name="md-close-circle" style={{fontSize: 30, color: '#34495e'}}/>
                 </Button>
             </View>
         );
-    }
+    };
 
     return (
         <Animated.View style={[styles.container, {opacity}]}>
@@ -42,8 +48,17 @@ const BaseLightbox = (props) => {
         </Animated.View>
     );
 }
-const styles = EStyleSheet.create({
-    container: {}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'rgba(52, 52, 52, .5)',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
 
 export default BaseLightbox;
